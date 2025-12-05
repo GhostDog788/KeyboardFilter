ba5ff7d95446385ad1acf59ce27eef014e7e5661
Dor <dorg50@gmail.com>
2025-12-05 20:40:46 +0200

    remove self from device extension

--- KeyboardFillter/Entry.cpp
+++ KeyboardFillter/Entry.cpp
@@ -4,7 +4,6 @@
 
 typedef struct _DEVICE_EXTENSION
 {
-	PDEVICE_OBJECT  Self;
 	PDEVICE_OBJECT  LowerDeviceObject;
 	// ... anything else you need
 } DEVICE_EXTENSION, * PDEVICE_EXTENSION;
@@ -20,6 +19,7 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 
 	UNICODE_STRING targetName = RTL_CONSTANT_STRING(L"\\Device\\KeyboardClass0");
 	NTSTATUS status = STATUS_SUCCESS;
+	PDEVICE_OBJECT filterDeviceObject = nullptr;
 	PDEVICE_EXTENSION devExt = nullptr;
 	do {
 		for (int i = 0; i < ARRAYSIZE(DriverObject->MajorFunction); i++) {
@@ -35,25 +35,23 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 		}
 		DriverObject->DriverUnload = DriverUnload;
 
-		PDEVICE_OBJECT filterDeviceObject = nullptr;
 		status = IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION), nullptr,
 			FILE_DEVICE_UNKNOWN, 0, FALSE, &filterDeviceObject);
 		if (!NT_SUCCESS(status)) break;
 		devExt = (PDEVICE_EXTENSION)filterDeviceObject->DeviceExtension;
 		RtlZeroMemory(devExt, sizeof(*devExt));
-		devExt->Self = filterDeviceObject;
 
 		PFILE_OBJECT FileObject;
 		PDEVICE_OBJECT TargetDeviceObject;
 		status = IoGetDeviceObjectPointer(&targetName, FILE_READ_DATA, &FileObject, &TargetDeviceObject);
 		if (!NT_SUCCESS(status)) break;
 
-		devExt->Self->Flags |= TargetDeviceObject->Flags & (DO_BUFFERED_IO | DO_DIRECT_IO);
-		devExt->Self->Flags &= ~DO_DEVICE_INITIALIZING;
-		devExt->Self->Flags |= DO_POWER_PAGABLE;
-		devExt->Self->DeviceType = TargetDeviceObject->DeviceType;
+		filterDeviceObject->Flags |= TargetDeviceObject->Flags & (DO_BUFFERED_IO | DO_DIRECT_IO);
+		filterDeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+		filterDeviceObject->Flags |= DO_POWER_PAGABLE;
+		filterDeviceObject->DeviceType = TargetDeviceObject->DeviceType;
 
-		status = IoAttachDeviceToDeviceStackSafe(devExt->Self, TargetDeviceObject, &devExt->LowerDeviceObject);
+		status = IoAttachDeviceToDeviceStackSafe(filterDeviceObject, TargetDeviceObject, &devExt->LowerDeviceObject);
 		ObfDereferenceObject(FileObject);
 		if (!NT_SUCCESS(status)) break;
 
@@ -62,8 +60,8 @@ DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	
 	if (!NT_SUCCESS(status)) {
 		log("Failed to attach to %wZ: status=0x%08X\n", &targetName, status);
-		if (devExt && devExt->Self) {
-			IoDeleteDevice(devExt->Self);
+		if (filterDeviceObject) {
+			IoDeleteDevice(filterDeviceObject);
 		}
 	}
 	return status;
